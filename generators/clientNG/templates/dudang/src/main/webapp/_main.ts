f47f27625f0b95345b1bce9408674b400ab40254
munifgebara <munifgebara@gmail.com>
2017-09-08 20:46:39 -0300

    PrimeNG

--- generators/clientNG/templates/dudang/src/main/webapp/_main.ts
+++ generators/clientNG/templates/dudang/src/main/webapp/_main.ts
@@ -1,3 +1,6 @@
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/switchMap';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
--- generators/clientNG/templates/dudang/src/main/webapp/app/_app.module.ts
+++ generators/clientNG/templates/dudang/src/main/webapp/app/_app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule, Http } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AccordionModule, GrowlModule, DataListModule } from 'primeng/primeng';
+import { ToolbarModule, ButtonModule, SplitButtonModule } from 'primeng/primeng';
+import { PanelModule } from 'primeng/primeng';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +16,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule, GrowlModule, DataListModule, ToolbarModule, ButtonModule, SplitButtonModule, PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private http: Http) {
+  }
+
+}
